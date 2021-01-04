2a58b52489a135d3fd59c9b832d9a39c7d36332f
Amit Patel <amit.patel1@spglobal.com>
2021-01-04 15:45:09 -0500

    Removed unwanted files

--- vars/jenkinslib.groovy
+++
@@ -1,7 +0,0 @@
-import com.two57.jenkins.sharedlib.JenkinsBuilder
-
-JenkinsBuilder call(def scriptReference, String projectName) {
-    return new JenkinsBuilder(scriptReference,projectName,env['BRANCH_NAME'])
-}
-
-return this
